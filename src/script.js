3bf477eb20caf4e31532a957cb1cdc34b0530f6a
48exa <douwewesterdijk@gmail.com>
2023-01-18 23:50:00 +0100

    key now turns red when you click it

--- src/script.js
+++ src/script.js
@@ -8,4 +8,14 @@ buttons.forEach(item => {
 function changeNotebox() {
     let octave = this.parentNode.parentNode.className.replace('octave-','');
     notebox.innerText = this.innerText + octave;
+    notebox.style.marginLeft = "25%"
+        
+    noteArray = Array.from(buttons)
+    for (let i = 0; i < noteArray.length; i++) {
+        if (noteArray[i].innerText + noteArray[i].parentNode.parentElement.className.replace('octave-','') == notebox.innerText) {
+            noteArray[i].classList.add("red")
+        } else {
+            noteArray[i].classList.remove("red")
+        }
+    }
 }
